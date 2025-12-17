4560a4864f1f3817e5d229b2a7575a87fe0300ff
Gabriel Potje <gpotje.souza@gmail.com>
2025-12-17 15:32:19 -0300

    3

--- Modulo 4/ex_194_funcao.py
+++ Modulo 4/ex_194_funcao.py
@@ -10,6 +10,11 @@
 # desfazer = [] -> Refazer ['caminhar', 'fazer café']
 # refazer = todo ['fazer café']
 
+import json
+
+lista_tarefas = []
+lista_tarefas_clone  = []
+
 def desenha_menu():
    print("=================================")
    print('Digite uma tarefa ou comando:')
@@ -17,30 +22,56 @@ def desenha_menu():
    print("=================================")
    print()
 
-def listar(lista):
+def gravaJson(lista,local):
+    dados_python = dictJson()
+    dados_python[local] = lista
+    with open('dados.json', 'w', encoding='utf-8') as arquivo:
+        json.dump(dados_python, arquivo, ensure_ascii=False, indent=4)
+
+def dictJson():
+    with open('dados.json', 'r', encoding='utf-8') as arquivo:
+        dados_python = json.load(arquivo)
+        return dados_python
+
+def listaJson():
+    with open('dados.json', 'r', encoding='utf-8') as arquivo:
+        dados_python = json.load(arquivo)
+        lista_tarefas = dados_python['comandos']
+        return lista_tarefas
+
+
+def listar():
+    lista_local = listaJson()
     print()
     print("TAREFAS:")
-    if not lista:
+    if not lista_local:
         print()
         print("=== A lista está vazia! Inseria um item ===")
     else:
-        for tarefa in lista:
+        for tarefa in lista_local:
             print(tarefa)
     print()
 
-def desfazer(lista_tarefas,lista_tarefas_clone):
+def desfazer():
+    lista_tarefas = listaJson()
     if not lista_tarefas:
         print("A lista está vazia!")
     else:
         lista_tarefas_clone.append(lista_tarefas[-1])
         del lista_tarefas[-1]
+        gravaJson(lista_tarefas,'comandos')
 
-def refazer(lista_tarefas,lista_tarefas_clone):
+
+
+
+def refazer():
+    lista_tarefas = listaJson()
     if not lista_tarefas_clone:
         print("A lista está vazia!")
     else:
         lista_tarefas.append(lista_tarefas_clone[-1])
         del lista_tarefas_clone[-1]
+        gravaJson(lista_tarefas,'refazer')
 
 
 
--- Modulo 4/ex_194_refazer.py
+++ Modulo 4/ex_194_refazer.py
@@ -12,31 +12,34 @@
 import os
 from ex_194_funcao import *
 
-lista_tarefas  = []
+
+lista_tarefas = []
 lista_tarefas_clone  = []
 
-tafera_comando = ''
 
 while True:
+
+
    desenha_menu()
    tafera_comando = input()
 
    if tafera_comando == 'listar':
-        listar(lista_tarefas)
+        listar()
 
    elif tafera_comando == 'clean':
        os.system("cls")
 
    elif tafera_comando == 'desfazer':
-       desfazer(lista_tarefas,lista_tarefas_clone)
-       listar(lista_tarefas)
+       desfazer()
+       listar()
 
    elif tafera_comando == 'refazer':
-       refazer(lista_tarefas,lista_tarefas_clone)
-       listar(lista_tarefas)
+       refazer()
+       listar()
 
    else:
      lista_tarefas.append(tafera_comando)
+     gravaJson(lista_tarefas,'comandos')
 
 
 
